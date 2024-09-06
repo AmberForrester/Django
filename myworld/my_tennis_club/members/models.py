c1e77ccfb5b352063319b2381d9f338c873cd78a
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-09-06 10:20:49 -0400

    Django Vies, URLs, Templates, Models, and Data Management.

--- myworld/my_tennis_club/members/models.py
+++ myworld/my_tennis_club/members/models.py
@@ -5,3 +5,5 @@ from django.db import models
 class Member(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
