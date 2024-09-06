f2ad6043be68a8a9bfad56ff887298a1d1e36861
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-09-06 11:09:49 -0400

    Display Data

--- myworld/my_tennis_club/members/views.py
+++ myworld/my_tennis_club/members/views.py
@@ -2,8 +2,15 @@
 
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member # Import the Member model
 
 def members(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    mymembers = Member.objects.all().values() # Creates a mymembers object with all the values of the Member model
+    
+    template = loader.get_template('all_members.html') # Loads the all_members.html template
+    
+    context = {
+        'mymembers': mymembers, # Creates an object containing the mymembers object
+    }
+    return HttpResponse(template.render(context, request)) # Sends the object to the template, outputs the HTML that is rendered by the template. 
 
