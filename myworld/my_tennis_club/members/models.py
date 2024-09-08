f3f9dd9837c52c6c91718c6830aed1eb53505cd7
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-09-08 15:45:31 -0400

    Change the string representation of objects in Python

--- myworld/my_tennis_club/members/models.py
+++ myworld/my_tennis_club/members/models.py
@@ -7,3 +7,12 @@ class Member(models.Model):
     lastname = models.CharField(max_length=255)
     phone = models.IntegerField(null=True)
     joined_date = models.DateField(null=True)
+    
+# To change the display of Members Model in the Admin User Interface.
+# You can change the string representation, by defining the __str__() function of the Member Model.
+# at the end of the about data fields, add the following code block:
+# def __str__(self):
+#   return f'{self.firstname} {self.lastname}'
+# This is will diplay Member by first and last name. 
+
+# Defining a __str__() function is NOT a Django feature, it is how to change the string representation of objects in Python. 
