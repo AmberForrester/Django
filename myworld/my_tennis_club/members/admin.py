7599c265b0ee80a7a8b6380fb14a3162c71ea9a3
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-09-08 15:41:42 -0400

    Models included in Admin Interface

--- myworld/my_tennis_club/members/admin.py
+++ myworld/my_tennis_club/members/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here:
+from .models import Member
+
+# Control the fields to display by specifying them in a list_display property. 
+# 1st - create a MemberAdmin() class:
+class MemberAdmin(admin.ModelAdmin):
+# 2nd - specify the list_display tuple:
+    list_display = ('firstname', 'lastname', 'joined_date',)
+
+# Add MemberAdmin as an argument. 
+admin.site.register(Member, MemberAdmin)
