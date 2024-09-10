d61a6220939607d66a4c9be7b19bf85f900c957d
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-09-09 20:47:21 -0400

    JobZila Django Version

---
+++ jobzila/jobzila/home/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.homepage, name='homepage'), # Map the URL http://127.0.0.1:8000/ to your homepage view. 
+    # Now, include the URLs from the home app in the project-level urls.py located in jobzila/urls.py. 
+    
+    #2nd step - Create the URL route for the contact us page. So when users visit http://127.0.0.1:8000/contact/ they are directed to the contact us page.
+    path('contact/', views.contact_us, name='contact_us'),
+]
---
+++ jobzila/jobzila/home/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+# Created a simple function that returns a homepage response:
+def homepage(request):
+    return render(request, 'home/index.html')
+
+# This tells Django to look for an index.html template inside a home/templates/home/ folder. 
+
+# 1st step - create a view for the Contact Us Page that will render the HTML template.
+def contact_us(request):
+    
+    # Handling form submission - if you want to process the form (ex. send an email or save the data), youll need to update the function here. 
+    # For now let us diplay a success message when the form is submitted: 
+    if request.method == 'POST':
+        # Here you can process the form (e.g., save the data or send an email)
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        
+        # Return simple success message:
+        return HttpResponse(f'Thanks for reaching out, {name}! We will get back to you within 24 hours.')
+    
+    return render(request, 'home/contactus.html')
